4956882779d1df2161327ade12de4bb94adb0276
Heather Cherewaty <hr.cherewaty@gmail.com>
2019-01-17 17:25:24 -0800

    adds setup of api and auth server

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# API-Server
+![CF](http://i.imgur.com/7v5ASc8.png) LAB
+=================================================
+
+## API Project
+
+### Author: Becca Lee, Heather Cherewaty, Jared Pattison
+
+### Links and Resources
+
+* [repo](https://github.com/hcherewaty/API-Server)
+* [travis]()
+* [heroku]()
+
+
+#### Documentation
+* [swagger](http://xyz.com) (API assignments only)
+* [jsdoc](http://xyz.com) (All assignments)
+
+### Modules
+#### `modulename.js`
+##### Exported Values and Methods
+
+
+### Setup
+#### `.env` requirements
+* `PORT` - Defined in ENV.
+* `MONGODB_URI` - Defined in ENV.
+
+#### Running the app
+* `npm start`
+* Endpoint: `/foo/bar/`
+  * Returns a JSON object with abc in it.
+  
+#### Tests
+* How do you run tests?
+* What assertions were made?
+* What assertions need to be / should be made?
+
+#### UML
+Link to an image of the UML for your application and response to events
---
+++ src/app.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/**
+ * API Server Module
+ * @module src/app
+ */
+
+const cwd = process.cwd();
+
+// 3rd Party Resources
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// Esoteric Resources
+const errorHandler = require( `${cwd}/src/middleware/500.js`);
+const notFound = require( `${cwd}/src/middleware/404.js` );
+const v1Router = require( `${cwd}/src/api/v1.js` );
+// const authRouter = require( './auth/router.js' ); ???
+
+// Prepare the express app
+const app = express();
+
+// App Level MW
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// Static Routes
+app.use('/docs', express.static('docs'));
+
+// Routes
+app.use(v1Router);
+// app.use(authRouter); ???
+
+// Catchalls
+app.use(notFound);
+app.use(errorHandler);
+
+/**
+ * Start Server on specified port
+ * @param port {integer} (defaults to process.env.PORT)
+ */
+let start = (port = process.env.PORT) => {
+  app.listen(port, () => {
+    console.log(`Server Up on ${port}`);
+  });
+};
+
+// module.exports = { ???
+//   server: app,
+//   start: (port) => {
+//     if( ! isRunning ) {
+//       app.listen(port, () => {
+//         isRunning = true;
+//         console.log(`Server Up on ${port}`);
+//       });
+//     }
+//     else {
+//       console.log('Server is already running');
+//     }
+//   },
+// };
+  
+module.exports = {app,start};
